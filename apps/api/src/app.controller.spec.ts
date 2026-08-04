a1b2c95cef2c2cd4a6bfffdc542c66d4e37b83e9
Elonatech-Projects <elonatechprojects@elonatech.com.ng>
2026-08-04 12:29:56 +0100

    fix: disable no-unsafe-assignment for entire test file, not just mock block

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
